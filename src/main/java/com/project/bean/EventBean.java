3eb30a853829fbb2a2917c1baf4c82ca9bf8b754
86132 <123456>
2020-06-15 21:27:49 +0800

    @data注解，区域，小班，事件实体类

--- src/main/java/com/project/bean/EventBean.java
+++ src/main/java/com/project/bean/EventBean.java
@@ -1,4 +1,28 @@
 package com.project.bean;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class EventBean {
+    private Integer eventId;
+
+    private Date date;
+
+    private String prevention;
+
+    private Integer state;
+
+    private String describe;
+
+    private float loss;
+
+    private float area;
+
+    private Integer pathWay;
+
+    private Integer disasterType;
+
+    private String picture;
 }

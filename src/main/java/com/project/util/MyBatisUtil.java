92fc6abf93a5783366867ddb8478e0764620e6b4
86132 <123456>
2020-06-15 14:08:47 +0800

    数据库配置和数据库驱动

---
+++ src/main/java/com/project/util/MyBatisUtil.java
@@ -0,0 +1,43 @@
+package com.project.util;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory = null;
+
+    static {
+        String resource = "mybatis-config.xml";
+        InputStream is = null;
+        // 加载配置文件
+        try {
+            is = Resources.getResourceAsStream(resource);
+            // 建造者模式
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //获取SqlSession对象（mybatis的连接对象，可以理解为JDBC的connection对象）
+    public static SqlSession getSession() {
+        return sqlSessionFactory.openSession();
+    }
+
+    public static void main(String[] args) {
+        SqlSession session = getSession();
+        System.out.println(session);
+    }
+}

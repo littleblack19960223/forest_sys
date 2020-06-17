27148cb4a1a598e4cce80950db6d96351b6b8dd4
quanzhou88 <1334409562@qq.com>
2020-06-17 13:42:51 +0800

    quanzhou6174

---
+++ src/main/java/com/project/servlet/experts/ExpertsIncidentServlet.java
@@ -0,0 +1,37 @@
+package com.project.servlet.experts;
+
+import com.github.pagehelper.PageInfo;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.project.bean.EventBean;
+import com.project.service.impl.ExpertsServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ExpertsIncidentServlet")
+/**展示专家需要解决的事件*/
+public class ExpertsIncidentServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //分页的数据
+        String curentPage = request.getParameter("curentPage");
+        String pageSize = request.getParameter("pageSize");
+        ExpertsServiceImpl expertsService = new ExpertsServiceImpl();
+        PageInfo<EventBean> list=expertsService.Incident(curentPage, pageSize);
+        GsonBuilder gsonBuilder = new GsonBuilder().setDateFormat("yyyy-MM-dd");
+        Gson gson =gsonBuilder.create();
+        String json = gson.toJson(list);
+        response.getWriter().print(json);
+
+    }
+}
---
+++ src/main/java/com/project/servlet/experts/addTalksServlet.java
@@ -0,0 +1,41 @@
+package com.project.servlet.experts;
+
+import com.project.bean.DiscussBean;
+import com.project.service.impl.ExpertsServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "addTalksServlet",value = "addTalks")
+/**添加会商结果信息*/
+public class addTalksServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //获得专家的字符串数组,进行分隔
+        String experts = request.getParameter("experts");
+        String[] split = experts.split(",");
+
+
+        //获得会商结果
+        String talksTxt = request.getParameter("talksTxt");
+        //获得当前时间
+        LocalDate now = LocalDate.now();
+        Date date = Date.valueOf(now);
+        DiscussBean discussBean = new DiscussBean(date, new String(), talksTxt);
+        ExpertsServiceImpl expertsService = new ExpertsServiceImpl();
+       int i = expertsService.addTalks(discussBean);
+
+    }
+}

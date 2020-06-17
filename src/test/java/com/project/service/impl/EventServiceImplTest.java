958c3e3b716d10cb7f0305df7170d7357c2ebb4b
86132 <123456>
2020-06-17 14:26:33 +0800

    事件1

--- src/test/java/com/project/service/impl/EventServiceImplTest.java
+++ src/test/java/com/project/service/impl/EventServiceImplTest.java
@@ -13,31 +13,40 @@ public class EventServiceImplTest {
 
     @Test
     public void addEvent() {
+
     }
 
     @Test
     public void getEventById() {
+        System.out.println(service.getEventById(1));
     }
 
     @Test
     public void updateEvent() {
+        System.out.println(service.updateEvent(1,2,"修改"));
     }
 
     @Test
     public void updateEventType() {
+        System.out.println(service.updateEventType(1,2));
     }
 
     @Test
     public void getEventByItem() {
+
     }
 
     @Test
     public void getAllEvent() {
         List<EventBean> list = service.getAllEvent();
         System.out.println(list);
+        System.out.println(list.size());
     }
 
     @Test
     public void getEventByType() {
+        List<EventBean> list = service.getEventByType();
+        System.out.println(list);
+        System.out.println(list.size());
     }
 }

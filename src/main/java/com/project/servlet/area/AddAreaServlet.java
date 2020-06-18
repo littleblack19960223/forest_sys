36ab65d0b0cc8b6e2e0260bce95f9ed24d886465
86132 <123456>
2020-06-18 10:59:03 +0800

    修改sql語句

--- src/main/java/com/project/servlet/area/AddAreaServlet.java
+++ src/main/java/com/project/servlet/area/AddAreaServlet.java
@@ -1,5 +1,9 @@
 package com.project.servlet.area;
 
+import com.project.bean.AreaBean;
+import com.project.service.IAreaService;
+import com.project.service.impl.AreaServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +11,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "AddAreaServlet")
+@WebServlet(name = "AddAreaServlet",value="/addArea")
 public class AddAreaServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IAreaService service = new AreaServiceImpl();
+        String areaName = request.getParameter("name");
+        String tree = request.getParameter("tree");
+        String goodTree = request.getParameter("goodTree");
+        Integer tpye = Integer.valueOf(request.getParameter("type"));
+
+        AreaBean areaBean = new AreaBean();
+        areaBean.setName(areaName);
+        areaBean.setGoodTree(goodTree);
+        areaBean.setType(tpye);
+        areaBean.setTree(tree);
+
+        int i = service.addArea(areaBean);
+
+        if (i == 1){
+            response.getWriter().println("成功");
+        }else {
+            response.getWriter().println("失败");
+        }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
--- src/main/java/com/project/servlet/area/GetAllAreaServlet.java
+++ src/main/java/com/project/servlet/area/GetAllAreaServlet.java
@@ -1,19 +1,34 @@
 package com.project.servlet.area;
 
+import com.google.gson.Gson;
+import com.project.bean.AreaBean;
+import com.project.service.IAreaService;
+import com.project.service.impl.AreaServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "GetAllAreaServlet")
+@WebServlet(name = "GetAllAreaServlet",value = "/getAllArea")
 public class GetAllAreaServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IAreaService service = new AreaServiceImpl();
+        List<AreaBean> list = service.getAllArea();
 
+        System.out.println(list.size());
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(list);
+
+        response.getWriter().print(json);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }

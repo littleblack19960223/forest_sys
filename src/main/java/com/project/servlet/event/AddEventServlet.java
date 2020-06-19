ee0cf1ef777400631891a7623f39ccf3250888a4
86132 <123456>
2020-06-19 16:40:07 +0800

    end 小班，区域，事件

--- src/main/java/com/project/servlet/event/AddEventServlet.java
+++ src/main/java/com/project/servlet/event/AddEventServlet.java
@@ -1,19 +1,73 @@
 package com.project.servlet.event;
 
+import com.project.bean.AreaBean;
+import com.project.bean.EventBean;
+import com.project.service.IEventService;
+import com.project.service.impl.EventServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 
-@WebServlet(name = "AddEventServlet", value = "/addEvent")
+
+@WebServlet(name = "AddEventServlet",value="/addEvent")
 public class AddEventServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
 
+
+        String name = request.getParameter("name");
+
+        Date date = Date.valueOf(request.getParameter("date"));
+
+        Integer state = Integer.valueOf(request.getParameter("state"));
+
+        String describe = request.getParameter("describe");
+
+        float loss = Float.parseFloat(request.getParameter("loss"));
+
+        float area = Float.parseFloat(request.getParameter("area"));
+
+        String prevention = request.getParameter("prevention");
+
+        Integer pathWay = Integer.valueOf(request.getParameter("pathWay"));
+
+        Integer disasterType = Integer.valueOf(request.getParameter("disasterType"));
+
+        AreaBean areaBean = new AreaBean();
+
+        Integer areaId = Integer.valueOf(request.getParameter("areaBean"));
+
+        areaBean.setAreaId(areaId);
+
+        String picture = request.getParameter("picture");
+
+        EventBean eventBean = new EventBean();
+        eventBean.setState(state);
+        eventBean.setPrevention(prevention);
+        eventBean.setPathWay(pathWay);
+        eventBean.setDisasterType(disasterType);
+        eventBean.setName(name);
+        eventBean.setDate(date);
+        eventBean.setAreaBean(areaBean);
+        eventBean.setDescribe(describe);
+        eventBean.setLoss(loss);
+        eventBean.setPicture(picture);
+        eventBean.setArea(area);
+        eventBean.setType(2);
+
+        IEventService service = new EventServiceImpl();
+
+        int i = service.addEvent(eventBean);
+
+        System.out.println(i);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
--- src/test/java/com/project/service/impl/EventServiceImplTest.java
+++ src/test/java/com/project/service/impl/EventServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.project.service.impl;
 
+import com.project.bean.AreaBean;
 import com.project.bean.EventBean;
 import com.project.service.IEventService;
 import org.junit.Test;
 
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +16,24 @@ public class EventServiceImplTest {
 
     @Test
     public void addEvent() {
+        EventBean eventBean = new EventBean();
+        eventBean.setState(1);
+        eventBean.setPrevention("prevention");
+        eventBean.setPathWay(1);
+        eventBean.setDisasterType(2);
+        eventBean.setName("name");
+        eventBean.setDate(new Date(System.currentTimeMillis()));
+        AreaBean areaBean = new AreaBean();
+        areaBean.setAreaId(7);
+        eventBean.setAreaBean(areaBean);
+        eventBean.setDescribe("describe");
+        eventBean.setLoss(Float.valueOf("100.00"));
+        eventBean.setPicture("/forest_sys/upload/1592553088299-3732910603041649112.jpg");
+        eventBean.setArea(Float.valueOf("100.00"));
+        eventBean.setType(2);
+        int i = service.addEvent(eventBean);
 
+        System.out.println(i);
     }
 
     @Test
